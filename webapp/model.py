071349bbb9693cb7d0575fb44118d894fea03801
Valeriya Pichugina <valeriya438@list.ru>
2019-06-20 22:25:14 +0300

    Добавила миграции и user login

--- webapp/model.py
+++ webapp/model.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -38,4 +39,23 @@ class Event(db.Model):
         return '<Event {} {} {} {} {} {} {} {}>'.format(self.event_name, 
         self.date_start, self.date_finish, self.country_id, self.type_id, 
         self.flight, self.meals, self.accommodation)
-        
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+    email = db.Column(db.String(50), unique=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)

cb82379ac39dbd0de4031936d33c09e7123c36a1
Alexander Medvedev <32nine@gmail.com>
2019-07-19 23:48:12 +0300

    Опять пришлось клонровать мастер( добавил комментарии, но не работает валидация

--- webapp/event/forms.py
+++ webapp/event/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, DateField, StringField, SubmitField, SelectField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms import BooleanField, HiddenField, DateField, StringField, SubmitField, SelectField, IntegerField
+from wtforms.validators import DataRequired, ValidationError
 from wtforms.fields.html5 import DateField
 
 class EventForm(FlaskForm):
@@ -49,3 +49,13 @@ class EventForm(FlaskForm):
         render_kw={"class": "form-check-input"}
     )
     submit = SubmitField('Отправить',  render_kw={"class": "btn btn-primary"})
+
+
+class CommentForm(FlaskForm):
+    event_id = HiddenField('ID события', validators=[DataRequired()])
+    comment_text = StringField('Текст комментария', validators=[DataRequired()], render_kw={"class": "form-control"})
+    submit = SubmitField('Отправить!', render_kw={"class": "btn btn-primary"})
+
+    def validate_event_id(self, event_id):
+        if not Event.query.get(event_id.data):
+            raise ValidationError('События с таким id не существует')
--- webapp/event/models.py
+++ webapp/event/models.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
 from flask import url_for
-from datetime import date
+from datetime import datetime
+from sqlalchemy.orm import relationship
 
 from webapp.db import db
 
@@ -55,3 +56,24 @@ class Event(db.Model):
     def delete_link(self):
         if self.id:
             return url_for('event.delete_event', ev_id=self.id)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    event_id = db.Column(
+        db.Integer,
+        db.ForeignKey('event.id', ondelete='CASCADE'),
+        index=True
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+
+    event = relationship('Event', backref='comments')
+    user = relationship('User', backref='comments')
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
